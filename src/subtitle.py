c0a72ecc5ea3d5f6ec5c0fa3fa5de1542152daae
GyuhoLee <gyuho965@gmail.com>
2020-11-02 17:10:59 +0900

    [Fix] 추출한 자막 -> xml 관련 eroor 해결

--- src/subtitle.py
+++ src/subtitle.py
@@ -1,8 +1,8 @@
 from pytube import YouTube
 
-video_url = 'https://www.youtube.com/watch?v=Zg3j6anDU6U'
+video_url = 'https://www.youtube.com/watch?v=ecUWKU_v318'
 yt = YouTube(video_url)
+title = yt.title
+description = yt.description
 caption = yt.captions.get_by_language_code('ko')
-if(caption == None):
-    caption = yt.captions.all()[0]
-caption.xml_captions()
+caption_xml = caption.xml_captions
